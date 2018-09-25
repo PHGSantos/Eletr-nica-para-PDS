4ed59b457129fa531893a589229fa21af923ee6d
sarahecomp <sarahecomp@gmail.com>
2018-09-24 21:23:31 -0300

    Adicionando arquivos

---
+++ sbc-template.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+
+plt.plot( [10,5,3,4,6,8] )
+plt.title("Muito F\unhbox \voidb@x \bgroup \let \unhbox \voidb@x \setbox \@tempboxa \hbox {a\global \mathchardef \accent@spacefactor \spacefactor }\accent 19 a\egroup \spacefactor \accent@spacefactor cil")
+plt.show()
+
+
